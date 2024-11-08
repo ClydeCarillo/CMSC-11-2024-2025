8f081a297bc0717e801232ae1d589349bb0ff8be
Clyde <75878247+ClydeCarillo@users.noreply.github.com>
2024-11-08 15:22:42 +0800

    Update MACHINE PROBLEM #6.1 - Germain Primes.c

--- MACHINE PROBLEM #6.1 - Germain Primes.c
+++ MACHINE PROBLEM #6.1 - Germain Primes.c
@@ -24,3 +24,59 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+// Without OOP version
+#include <stdio.h>
+
+int main() {
+    int n, prime = 1, i, n2;
+
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        prime = 0;
+    } 
+    else {
+        i = 2;
+        while (i * i <= n) {
+            if (n % i == 0) {
+                prime = 0; 
+            }
+            i++;
+        }
+    }
+
+    // If n is prime, check if 2*n + 1 is also prime
+    if (prime == 1) {
+        n2 = 2 * n + 1; // Calculate 2*n + 1
+        prime = 1; // Assume n2 is prime
+        if (n2 <= 1) {
+            prime = 0; // n2 is not prime if it's <= 1
+        } else {
+            i = 2;
+            while (i * i <= n2) {
+                if (n2 % i == 0) {
+                    prime = 0; // n2 is divisible by i, so it's not prime
+                }
+                i++;
+            }
+        }
+    }
+    
+    // Output whether n is a Germain Prime or not
+    if (prime == 1) {
+        printf("%d is a Germain Prime Number.\n", n);
+    } else {
+        printf("%d is NOT a Germain Prime Number.\n", n);
+    }
+    return 0;
+}
